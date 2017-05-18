acfdb8b44554a98dd6fa34ffe8878dd5dd03c417
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:37:05 -0700

    functional-shared queue added

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  
+  obj.data = {};
+
+  obj.sizeCounter = 0;
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.sizeCounter;
+  },
+  enqueue: function(value) {
+    this.data[this.sizeCounter] = value;
+    this.sizeCounter++;
+  },
+  dequeue: function() {
+    if (this.sizeCounter !== 0 ) {
+      this.sizeCounter--;      
+    }
 
+    var dequeueValue = this.data[0];
+    
+    for (var key in this.data) {
+      if (key >= 1) {
+        this.data[key - 1] = this.data[key];
+      }
+    }
 
+    this[this.sizeCounter] = undefined;
+    
+    return dequeueValue;
+  }
+};
