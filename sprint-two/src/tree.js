b4bc0407bd487aa8b7e91c5b1b420e4486378092
Hack Reactor Student <student@hackreactor.com>
2017-05-18 21:29:00 -0700

    Finish tree class in functional with shared methods style

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,22 +2,45 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) {  
+  // generate a child node & push into the paraent.children
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // debugger;
+  let childNode = {}; 
+  // iterate through children node
+  for (let i = 0; i < this.children.length; i++) {
+    childNode = this.children[i];
+    // check if the child.value is target
+    if (childNode.value === target) {
+      return true;
+    // else if the current node has children
+    } else if (childNode.children.length > 0) {
+      // call contains function recursively
+      if (childNode.contains(target)) {
+        //return true only if target matches
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * O(n) for contains
+ * assuming we know the index of the child, 
+ * O(1) for addChild. 
  */
